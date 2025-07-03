93aad9c94f5fe7a19f825e5616bfc3c49bffd1eb
AntoineTouchard <antoine.touchard@anyti.me>
2025-07-03 09:50:00 +0200

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,20 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api')
 
-  app.enableCors()
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Anonymous-Token',
+      'User-Agent',
+    ],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
 
   await app.listen(process.env.PORT ?? 3200)
 }
